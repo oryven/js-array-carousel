7b70f2534cb4151cc39a585a4db6584db798c3a5
Orazio <orazioventrella@hotmail.it>
2021-10-28 21:47:06 +0200

    creato ciclo for e stampa elementi in pagina

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+/* Dati tre array contenenti: - una lista ordinata di 5 immagini, - una lista ordinata dei relativi 5 luoghi e - una lista di 5 news, creare un carosello come nella foto allegata.
+ Al click dell’utente sulle frecce verso l’alto o verso il basso, l’immagine attiva diventa visibile in formato grande a sinistra e nel suo angolo in basso a destra dovranno essere aggiunti i relativi - titolo e - testo.
+ Allo stesso tempo nelle miniature l’immagine attiva dovrà apparire in evidenza rispetto alle altre.
+ */
+
 const items = [
     'img/01.jpg',
     'img/02.jpg',
@@ -21,3 +26,56 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+/*  1. creo le due costanti per richiare i div ch dovranno          contenere le immagini
+    2. creo le due variabili vuote verrano sovrascritte dagli itmes
+      2.1 creo un ciclo per inserire le immagini all'interno delle variabili
+    3. stampo nel dom gli elementi creati duranti il ciclio
+    4. creo una funzione con evento click
+       4.1 se clicco sulle freccie avanti e indietro per aggiungere o rimuovere la classe active
+*/
+
+// richiamo con delle variabili gli elementi che dovranno contenere le immagini
+
+let itemsCont = document.querySelector(".items");
+
+let thumbsCont = document.querySelector(".thumbs");
+
+// creo delle variabili vuote
+
+let contenutoItem = "";
+
+let contenutoThumb = "";
+
+// creo un ciclo per andare creare all'interno delle varibili i div
+
+for (let i = 0; i < items.length; i++){
+
+    contenutoItem +=`
+    <div class="item">
+        <img src="${items[i]}" alt="${title[i]}">
+        <div class="text">
+            <h3>${title[i]}</h3>
+            <p>${text[i]}</p>
+        </div>
+    </div>`;
+
+    contenutoThumb += `
+    <div class="thumb">
+        <img src="${items[i]}" alt="${title[i]}">
+    </div>`;
+
+}
+// stampo gli elementi nel DOM
+
+itemsCont.innerHTML += contenutoItem;
+
+document.getElementsByClassName("item")[0].classList.add("active");
+
+thumbsCont.innerHTML += contenutoThumb;
+
+document.getElementsByClassName("thumb")[0].classList.add("active");
+
+// creo una funzione con un evento "click" con all'interno una condizione per aggiungere e rimuovere la classe active
+
+
